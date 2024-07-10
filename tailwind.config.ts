e1aa9d64da6964f4dcaa43bce03318a45713a17c
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-10 14:13:40 +0300

    update: add colors library to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,12 @@ export default {
         '90': '22.5rem',
         '160': '40rem',
         '320': '80rem'
+      },
+      colors: {
+        'inno-green': '#40BA21',
+        'lightgray': '#F0F3F7',
+        'gray': '#999FAF',
+        'darkblue': '#12152A'
       }
     }
   }
