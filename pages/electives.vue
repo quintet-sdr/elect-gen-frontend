a642ba73df22259afcec2d6957806698c4c99d38
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-13 16:38:55 +0300

    add: start electives page

---
+++ pages/electives.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Header/>
+  <main class="flex flex-col items-center gap-4">
+    <h1 class="primary">Electives</h1>
+    <div class="flex flex-col items-center gap-4 w-full h-90 rounded-3xl border-color-lightgray dark:border-b-white">
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
