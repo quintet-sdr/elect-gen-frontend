f8757e744b9cda6b25dbfab2a8c859cc748ffbd2
Projacktor <1234xl.com@gmail.com>
2024-07-13 18:09:53 +0300

    feat: add ElectiveEdit.vue and update electives.vue page

---
+++ components/ElectiveEdit.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <article
+    class="flex flex-row flex-wrap items-center order-1 gap-40 rounded-xl bg-gray-300 px-3 py-2 text-base font-medium dark:text-color-darkblue"
+  >
+    {{ props.name }}
+  <button class="bg-transparent italic order-2">Edit</button>
+  </article>
+</template>
--- pages/electives.vue
+++ pages/electives.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-
+const electives = reactive([
+  'Advanced Programming in C/C++',
+  'Architecture of Computing Devices',
+  'Branding & Marketing in IT industry',
+  'Cross-platform Mobile Development with Flutter',
+  'Front-end Web Development',
+  'Introduction to 3D Modeling',
+  'Introduction to Electronic and Logic Circuits',
+  'Introduction to Mechanical Engineering',
+  'Introduction to Robotics Operating System: Basics, Motion, and Vision',
+  'Lambda-Calculus, Algebra, Machinery and Logic for Formal Program Semantics',
+  'Real-Time Scheduling Theory'
+])
 </script>
 
 <template>
   <Header/>
-  <main class="flex flex-col items-center gap-4">
+  <main class="flex flex-col items-center gap-20">
     <h1 class="primary">Electives</h1>
-    <div class="flex flex-col items-center gap-4 w-full h-90 rounded-3xl border-color-lightgray dark:border-b-white">
-        <Elective name="Advanced C++" description=""/>
+    <div class="flex flex-col items-center gap-4 w-full min-h-90 rounded-3xl border-color-lightgray dark:border-b-white">
+      <div
+    v-for="(elective, index) in electives"
+    :key="index"
+     class="">
+        <ElectiveEdit :name="elective"/>
+    </div>
+      <button class="flex justify-center items-center w-160 rounded-xl bg-gray-300 px-3 py-2 text-base font-medium dark:text-color-darkblue hover:border-color-darkblue">+</button>
     </div>
   </main>
 </template>
