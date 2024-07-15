abd708d1e2fbc4a1b6cdbba54e9ba0ea679bd62a
Projacktor <1234xl.com@gmail.com>
2024-07-15 13:12:21 +0300

    fix: fix imports after chore

--- pages/electives.vue
+++ pages/electives.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import { electives } from '~/constants/electivesList.js'
+import Header from "~/components/widgets/Header.vue";
 </script>
 
 <template>
-  <Header />
+  <Header/>
   <main class="flex flex-col items-center justify-around">
     <h1 class="primary">Electives</h1>
     <div class="flex flex-row items-center justify-around">
