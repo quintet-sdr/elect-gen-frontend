0b06385019df0fe08ae8fd6505db8ddecaec984f
Projacktor <1234xl.com@gmail.com>
2024-07-15 13:08:34 +0300

    chore: create  and  directories. move componets

--- pages/electives.vue
+++ pages/electives.vue
@@ -4,19 +4,14 @@ import { electives } from '~/constants/electivesList.js'
 
 <template>
   <Header />
-  <main class="flex flex-col items-center gap-20">
+  <main class="flex flex-col items-center justify-around">
     <h1 class="primary">Electives</h1>
-    <div
-      class="w-ffullull flex min-h-90 flex-col items-center gap-4 rounded-3xl border-color-lightgray dark:border-b-white"
-    >
-      <div class="" v-for="(elective, index) in electives" :key="index">
-        <ElectiveEdit :name="elective" />
+    <div class="flex flex-row items-center justify-around">
+      <div class="flex flex-col items-center">
+
       </div>
-      <button
-        class="flex w-width-100 items-center justify-center rounded-xl bg-gray-300 px-3 py-2 text-base font-medium hover:border-color-darkblue dark:text-color-darkblue"
-      >
-        +
-      </button>
+
+      <div></div>
     </div>
   </main>
 </template>
