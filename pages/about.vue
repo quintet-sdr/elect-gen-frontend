203b5081c9ba0eb6604d54628a2f1798bf657397
acecution <acecution7@gmail.com>
2024-07-17 21:42:54 +0300

    fix: fix css

--- pages/about.vue
+++ pages/about.vue
@@ -5,78 +5,49 @@
 <template>
     <Header />
     <main>
-        <div class="relative pt-20 pl-5 w-full h-full">
-            <div class="participant"><p class="name">Participant 1</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 2</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 3</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 4</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 5</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 6</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 7</p><p class="description">Front-end</p></div>
-            <div class="participant"><p class="name">Participant 8</p><p class="description">Front-end</p></div>
+      <div class="relative pt-20 pl-5 w-full h-full">
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center mt-[10rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Evgenii Bortsov</p>
+            <p class="text-center">UX/UI Designer</p>
         </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center mt-[10rem] ml-[90rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Anastasia Varfolomeeva</p>
+            <p class="text-center">Front-end</p>
+        </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center mt-[10rem] ml-[45rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Kirill Efimovich</p>
+            <p class="text-center">Algorithm Developer</p>
+        </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[25rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Ilya-Linh Nguen</p>
+            <p class="text-center">DevOps</p>
+        </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[65rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Maxim Malov</p>
+            <p class="text-center">Algorithm Developer</p>
+        </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[25rem] mt-[25rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Arsen Galiev</p>
+            <p class="text-center">Full-stack</p>
+        </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[65rem] mt-[25rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Arsen Arslanov</p>
+            <p class="text-center">Full-stack</p>
+        </div>
+        <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[45rem] mt-[35rem]">
+            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
+            <p class="font-bold text-center">Sarmat Lutfullin</p>
+            <p class="text-center">Front-end</p>
+        </div>
+      </div>
     </main>
-</template>
-
-<style scoped>
-    .heart-shape {
-        position: relative;
-        width: 100%;
-        height: 100%;
-    }
-
-    .participant {
-        position: absolute;
-        width: 8vw;
-        height: 8vw;
-        background-color: rgb(0, 0, 0);
-        border-radius: 50%;
-    }
-
-    .participant:nth-child(1) {
-        margin-top: 10em;
-    }
-
-    .participant:nth-child(2) {
-        margin-top: 10em;
-        margin-left: 45em;
-    }
-
-    .participant:nth-child(3) {
-        margin-top: 10em;
-        margin-left: 90em;
-    }
-
-    .participant:nth-child(4) {
-        margin-left: 25em;
-    }
-
-    .participant:nth-child(5) {
-        margin-left: 65em;
-    }
-
-    .participant:nth-child(6) {
-        margin-left: 25em;
-        margin-top: 25em;
-    }
-
-    .participant:nth-child(7) {
-        margin-left: 65em;
-        margin-top: 25em;
-    }
-
-    .participant:nth-child(8) {
-        margin-left: 45em;
-        margin-top:35em;
-    }
-
-    .name {
-        padding-top: 170px;
-        padding-left: 28px;
-        font-weight: bold;
-    }
-
-    .description {
-        padding-left: 38px;
-    }
-</style>
+  </template>
+  
+  <style scoped></style>
