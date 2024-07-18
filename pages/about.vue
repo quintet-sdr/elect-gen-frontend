f8b57f5dbbdc624a64c20dd7864eac34c685e845
acecution <acecution7@gmail.com>
2024-07-18 18:06:49 +0300

    feat: add links to gh accounts

--- pages/about.vue
+++ pages/about.vue
@@ -7,44 +7,76 @@
     <main>
       <div class="relative pt-20 pl-5 w-full h-full">
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center mt-[10rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Evgenii Bortsov</p>
-            <p class="text-center">UX/UI Designer</p>
+          <a href="https://github.com/ebortsov" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Evgenii Bortsov</p>
+          <p class="text-center">UX/UI Designer</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center mt-[10rem] ml-[90rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Anastasia Varfolomeeva</p>
-            <p class="text-center">Front-end</p>
+          <a href="https://github.com/acecution" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Anastasia Varfolomeeva</p>
+          <p class="text-center">Front-end</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center mt-[10rem] ml-[45rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Kirill Efimovich</p>
-            <p class="text-center">Algorithm Developer</p>
+          <a href="https://github.com/lanebo1" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Kirill Efimovich</p>
+          <p class="text-center">Algorithm Developer</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[25rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Ilya-Linh Nguen</p>
-            <p class="text-center">DevOps</p>
+          <a href="https://github.com/Pickpusha" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Ilya-Linh Nguen</p>
+          <p class="text-center">DevOps</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[65rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Maxim Malov</p>
-            <p class="text-center">Algorithm Developer</p>
+          <a href="https://github.com/newspec" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Maxim Malov</p>
+          <p class="text-center">Algorithm Developer</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[25rem] mt-[25rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Arsen Galiev</p>
-            <p class="text-center">Full-stack</p>
+          <a href="https://github.com/projacktor" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Arsen Galiev</p>
+          <p class="text-center">Full-stack</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[65rem] mt-[25rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Arsen Arslanov</p>
-            <p class="text-center">Full-stack</p>
+          <a href="https://github.com/asqarslanov" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Asqar Arslanov</p>
+          <p class="text-center">Full-stack</p>
         </div>
         <div class="absolute w-[8vw] h-[8vw] flex-col items-center ml-[45rem] mt-[35rem]">
-            <div class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%] bg-black"></div>
-            <p class="font-bold text-center">Sarmat Lutfullin</p>
-            <p class="text-center">Front-end</p>
+          <a href="https://github.com/1sarmatt" target="_blank" rel="noopener noreferrer">
+          <Icon
+            class="w-[8vw] h-[8vw] mb-[10px] rounded-[50%]" name="local:avatar2"
+          />
+          </a>
+          <p class="font-bold text-center">Sarmat Lutfullin</p>
+          <p class="text-center">Front-end</p>
         </div>
       </div>
     </main>
