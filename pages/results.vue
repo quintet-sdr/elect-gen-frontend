502f68f0b8c2d816731e6f7fe804316d3a8487b1
Projacktor <1234xl.com@gmail.com>
2024-07-19 01:22:09 +0300

    fix:

--- pages/results.vue
+++ pages/results.vue
@@ -7,15 +7,15 @@ import Header from '~/components/widgets/Header.vue';
     id: number
     course_codename: string
   }
-  
+
   const jsonData = {
     student_email: "i.nguen@innopolis.university",
     id: 1,
     course_codename: "Front-end Web Development"
   } as JsonData;
-  
+
   const formattedJson = computed(() => JSON.stringify(jsonData, null, 2));
-  
+
   function downloadJson(): void {
     const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsonData));
     const downloadAnchorNode = document.createElement('a');
@@ -30,12 +30,12 @@ import Header from '~/components/widgets/Header.vue';
 <template>
     <Header />
     <main class="flex flex-col items-center justify-center min-h-screen">
-        <h1 class="primary text-2xl text-color-darkblue dark:color-lightgray font-semibold mb-4">We're all done!</h1>
-        <div class="bg-black text-white w-full max-w-4xl h-[32rem] overflow-auto p-4 mb-4">
+        <h1 class="primary">We're all done!</h1>
+        <div class="bg-black text-white w-full h-[32rem] p-4 mb-4">
             <pre>{{ formattedJson }}</pre>
         </div>
-        <p class="text-xl font-bold text-color-darkblue dark:color-lightgray mb-4">If the download has not started click on the button</p>
-        <button @click="downloadJson" class="bg-green-500 text-white py-2 px-4 rounded-xl hover:bg-green-700">
+        <p class="text-xl font-bold text-color-darkblue dark:text-color-lightgray mb-4">If the download has not started click on the button</p>
+        <button @click="downloadJson" class="bg-color-inno-green text-color-lightgray py-2 px-4 rounded-xl hover:opacity-75">
             Download table
         </button>
     </main>
