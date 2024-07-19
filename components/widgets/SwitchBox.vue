a34a505cc265003372bf8df3211d277be844a0cb
Projacktor <1234xl.com@gmail.com>
2024-07-19 12:13:25 +0300

    feat: add creating new elective functionality to

--- components/widgets/SwitchBox.vue
+++ components/widgets/SwitchBox.vue
@@ -1,18 +1,74 @@
 <script lang="ts" setup>
-import { ref, defineEmits, defineProps, watch } from 'vue'
-import { techElectives, humElectives } from '~/constants/electivesList.js'
+import { ref, reactive, defineEmits, defineProps, watch } from 'vue'
 import ElectiveButton from '~/components/shared/Elective/ElectiveButton.vue'
 
-const props = defineProps<{ disabled: boolean }>()
-const emit = defineEmits(['elective-change'])
+let techElectives = reactive([
+  'Advanced Programming in C/C++',
+  'Architecture of Computing Devices',
+  'Branding & Marketing in IT industry',
+  'Cross-platform Mobile Development with Flutter',
+  'Front-end Web Development',
+  'Introduction to 3D Modeling',
+  'Introduction to Electronic and Logic Circuits',
+  'Introduction to Mechanical Engineering',
+  'Introduction to Robotics Operating System: Basics, Motion, and Vision',
+  'Lambda-Calculus, Algebra, Machinery and Logic for Formal Program Semantics',
+  'Real-Time Scheduling Theory',
+])
 
-const currentBlock = ref('block1')
-const activeElective = ref<string | null>(null)
+let humElectives = reactive([
+  'Personal Efficiency skills',
+  'Psychology of IT-specialist',
+  'Introduction to Career Development for IT-specialist',
+  'UX/UI Design',
+  'Public Speaking in Business',
+  'Reading skills for IT specialist',
+  'Career and Leadership',
+  'Applied Economics: Introduction to IT Entrepreneurship',
+  'Tech Startup Design',
+  'Basics of Product management',
+  'Design Thinking for IT-specialist'
+])
+
+const props = defineProps<{ disabled: boolean }>();
+const emit = defineEmits(['elective-change']);
+
+const currentBlock = ref('block1');
+const activeElective = ref<string | null>(null);
+
+const newElectiveName = ref('');
+const newElectiveEditing = ref(false);
 
 const handleElectiveClick = (elective: string) => {
   if (!props.disabled) {
     activeElective.value = elective
     emit('elective-change', elective)
+  }
+}
+
+const startEditingNewElective = () => {
+  newElectiveEditing.value = true;
+}
+
+const addNewElective = () => {
+  if (newElectiveName.value.trim() !== '') {
+    if (currentBlock.value === 'block1') {
+      techElectives.push(newElectiveName.value.trim());
+    } else if (currentBlock.value === 'block2') {
+      humElectives.push(newElectiveName.value.trim());
+    }
+    newElectiveName.value = '';
+    newElectiveEditing.value = false;
+  }
+}
+
+const handleBlur = () => {
+  addNewElective();
+}
+
+const handleKeyup = (event: KeyboardEvent) => {
+  if (event.key === 'Enter') {
+    addNewElective();
   }
 }
 
@@ -48,7 +104,7 @@ watch(
         Hum
       </button>
     </div>
-    <div class="flex w-full flex-col items-center justify-center">
+    <div class="flex w-full h-full flex-col items-center justify-center">
       <div class="flex flex-col items-center gap-8 text-center" v-if="currentBlock === 'block1'">
         <div class="flex flex-col items-center justify-around gap-4">
           <ElectiveButton
@@ -60,11 +116,23 @@ watch(
             @click="handleElectiveClick(elective)"
           />
         </div>
-        <button
-          class="flex w-90 items-center justify-center rounded-2xl border-2 bg-gray-300 text-2xl font-semibold text-color-darkblue hover:border-color-inno-green"
-        >
-          +
-        </button>
+        <div class="flex w-90 items-center justify-center rounded-2xl border-2 bg-gray-300 text-2xl font-semibold text-color-darkblue hover:border-color-inno-green">
+          <template v-if="newElectiveEditing">
+            <input
+              v-model="newElectiveName"
+              @blur="handleBlur"
+              @keyup="handleKeyup"
+              class="bg-transparent border-none focus:outline-none w-full text-center"
+              placeholder="Enter course name"
+            />
+          </template>
+          <template v-else>
+            <button class="w-full"
+              @click="startEditingNewElective">
+              +
+            </button>
+          </template>
+        </div>
       </div>
       <div class="flex flex-col items-center gap-8 text-center" v-if="currentBlock === 'block2'">
         <div class="flex flex-col items-center justify-around gap-4">
@@ -77,11 +145,22 @@ watch(
             @click="handleElectiveClick(elective)"
           />
         </div>
-        <button
-          class="flex w-90 items-center justify-center rounded-2xl border-2 bg-gray-300 text-2xl font-semibold text-color-darkblue hover:border-color-inno-green"
-        >
-          +
-        </button>
+        <div class="flex w-90 items-center justify-center rounded-2xl border-2 bg-gray-300 text-2xl font-semibold text-color-darkblue hover:border-color-inno-green">
+          <template v-if="newElectiveEditing">
+            <input
+              v-model="newElectiveName"
+              @blur="handleBlur"
+              @keyup="handleKeyup"
+              class="bg-transparent border-none focus:outline-none w-full text-center"
+              placeholder="Enter course name"
+            />
+          </template>
+          <template v-else>
+            <button @click="startEditingNewElective">
+              +
+            </button>
+          </template>
+        </div>
       </div>
     </div>
   </div>
