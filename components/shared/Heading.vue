5803326763d048a95f1f6fbb2eee56bf2ae4dd9d
Asqar Arslanov <asqarslanov@gmail.com>
2024-07-20 18:20:49 +0300

    feat: add levels to heading

--- components/shared/Heading.vue
+++ components/shared/Heading.vue
@@ -1,13 +1,19 @@
 <script lang="ts" setup>
 interface Props {
   text: string
+  level: number
 }
 
-const props = defineProps<Props>()
+const props = withDefaults(defineProps<Props>(), {
+  level: 1
+})
 </script>
 
 <template>
-  <h1 class="text-7xl font-bold">
+  <h1 class="text-7xl font-bold" v-if="props.level === 1">
     {{ props.text }}
   </h1>
+  <h2 class="text-4xl font-bold" v-if="props.level === 2">
+    {{ props.text }}
+  </h2>
 </template>
