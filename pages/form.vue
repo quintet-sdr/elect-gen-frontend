1ad2734a1070982b8ff7ab366e7b2d7d6ab5a651
Asqar Arslanov <asqarslanov@gmail.com>
2024-07-20 21:03:17 +0300

    feat: form page

--- pages/form.vue
+++ pages/form.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
+import Heading from '~/components/shared/Heading.vue'
 import * as api from '~/server/utils/api'
 import type { Course } from '~/server/utils/schemas'
 
-// import { techElectives } from '~/constants/electivesList.js'
-// import Elective from '~/components/shared/Elective/Elective.vue'
+definePageMeta({ layout: false })
 
 const NUM_OF_COURSES: number = 5
 
@@ -35,20 +35,67 @@ for (let i = 0; i < NUM_OF_COURSES; i += 1) {
   filteredCourses.value.push(computed(() => courses.value?.filter((it) => except(it, i))))
 }
 
-const fc = ref([{ id: 1, contents: 'hey' }])
+const items = [
+  { label: 'Tech', type: 'tech' },
+  { label: 'Hum', type: 'hum' }
+]
+
+const route = useRoute()
+const router = useRouter()
+
+function selectTab(value: number): void {
+  router.replace({
+    query: { type: items[value].type }
+  })
+}
+
+const selectedTab = computed({
+  get() {
+    const index = items.findIndex((item) => item.type === route.query.type)
+    if (index === -1) {
+      return 0
+    }
+    return index
+  },
+  set: selectTab
+})
+
+async function submit(): Promise<void> {}
+
+onMounted(() => selectTab(0))
 </script>
 
 <template>
-  <div v-for="i in 5">
-    <USelectMenu
-      v-model="selected[i - 1]"
-      :options="filteredCourses[i - 1].value"
-      :search-attributes="['full_name', 'short_name']"
-      by="id"
-      option-attribute="short_name"
-      searchable
-    />
-  </div>
+  <NuxtLayout name="default">
+    <template #header>
+      <UTabs class="w-32" v-model="selectedTab" :items="items" />
+    </template>
 
-  {{ enabled }}
+    <Heading class="text-color-accent" :level="2" text="Select your priorities" />
+
+    <div />
+
+    <form class="flex w-72 flex-col items-center gap-4" @submit.prevent="submit">
+      <UFormGroup class="w-full" v-for="i in 5" :label="`Priority ${i}`" required>
+        <USelectMenu
+          v-model="selected[i - 1]"
+          :options="filteredCourses[i - 1].value"
+          :search-attributes="['full_name', 'short_name']"
+          by="id"
+          option-attribute="short_name"
+          searchable
+        />
+      </UFormGroup>
+
+      <div />
+
+      <button
+        class="w-full rounded-lg bg-color-accent py-1.5 font-medium enabled:text-white disabled:cursor-not-allowed disabled:bg-color-surface disabled:text-color-overlay"
+        :disabled="!enabled"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  </NuxtLayout>
 </template>
