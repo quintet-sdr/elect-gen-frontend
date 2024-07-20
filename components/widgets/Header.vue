ccc4abccdc25e4e06a7343a48b6b3b5882bf17b2
Asqar Arslanov <asqarslanov@gmail.com>
2024-07-21 00:34:57 +0300

    feat: change header arrow

--- components/widgets/Header.vue
+++ components/widgets/Header.vue
@@ -1,12 +1,4 @@
 <script lang="ts" setup>
-// const { t } = useI18n({ useScope: 'local' })
-// const store = useStore()
-// const signOut = [[{ label: t('header.account.sign-out') }]]
-
-// function signOutAndNavigate() {
-//   navigateTo('/login')
-// }
-
 interface Props {
   back: boolean
 }
@@ -17,23 +9,22 @@ const props = withDefaults(defineProps<Props>(), {
 
 const iconName = props.back ? 'fa-solid:arrow-left' : 'local:innopolis-university'
 
-function navigateToPage() {
+function goBack() {
   navigateTo('/')
 }
 </script>
 
 <template>
   <header class="grid h-16 w-full grid-cols-3 items-center">
-    <div class="flex h-10 w-10 cursor-pointer items-center justify-center" @click="navigateToPage">
+    <button
+      class="flex h-10 w-10 cursor-pointer items-center justify-center text-color-overlay hover:text-color-text"
+      @click="goBack"
+    >
       <Icon class="size-6" :name="iconName" />
-    </div>
+    </button>
 
     <div class="justify-self-center">
       <slot />
     </div>
-
-    <!-- <UDropdown class="justify-self-end" :items="signOut" @click="signOutAndNavigate" mode="hover">
-      {{ store.email }}
-    </UDropdown> -->
   </header>
 </template>
