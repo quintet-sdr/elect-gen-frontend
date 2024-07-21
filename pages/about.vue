e8ce9270b5f3d07bf9a3a26313168e7f3c81d38f
Projacktor <1234xl.com@gmail.com>
2024-07-21 23:11:38 +0300

    feat: create teamMembers global variable

--- pages/about.vue
+++ pages/about.vue
@@ -1,73 +1,7 @@
 <script lang="ts" setup>
-import avatar1 from '~/assets/images/avatar1.jpg'
-import avatar2 from '~/assets/images/avatar2.jpg'
-import avatar3 from '~/assets/images/avatar3.jpg'
-import avatar4 from '~/assets/images/avatar4.jpg'
-import avatar5 from '~/assets/images/avatar5.png'
-import avatar6 from '~/assets/images/avatar6.jpg'
-import avatar7 from '~/assets/images/avatar7.jpg'
-import avatar8 from '~/assets/images/avatar8.jpg'
 import Member from '~/components/shared/Member.vue'
 import Header from '~/components/widgets/Header.vue'
-
-const teamMembers = [
-  {
-    image: avatar1,
-    alt: 'evgenii-photo',
-    link: 'https://github.com/ebortsov',
-    name: 'Evgenii Bortsov',
-    role: 'UX/UI Designer'
-  },
-  {
-    image: avatar2,
-    alt: 'anastasia-photo',
-    link: 'https://github.com/acecution',
-    name: 'Anastasia Varfolomeeva',
-    role: 'Front-end'
-  },
-  {
-    image: avatar3,
-    alt: 'kirill-photo',
-    link: 'https://github.com/lanebo1',
-    name: 'Kirill Efimovich',
-    role: 'Algorithm Developer'
-  },
-  {
-    image: avatar4,
-    alt: 'ilya-linh-sdr-photo',
-    link: 'https://github.com/Pickpusha',
-    name: 'Ilya-Linh Nguen',
-    role: 'DevOps'
-  },
-  {
-    image: avatar5,
-    alt: 'maxim-photo',
-    link: 'https://github.com/newspec',
-    name: 'Maxim Malov',
-    role: 'Algorithm Developer'
-  },
-  {
-    image: avatar6,
-    alt: 'arsen-photo',
-    link: 'https://github.com/projacktor',
-    name: 'Arsen Galiev',
-    role: 'Full-stack'
-  },
-  {
-    image: avatar7,
-    alt: 'asqar-photo',
-    link: 'https://github.com/asqarslanov',
-    name: 'Asqar Arslanov',
-    role: 'Full-stack'
-  },
-  {
-    image: avatar8,
-    alt: 'sarmat-photo',
-    link: 'https://github.com/1sarmatt',
-    name: 'Sarmat Lutfullin',
-    role: 'Front-end'
-  }
-]
+import {teamMembers} from "~/constants/teamMembers";
 
 useHead({
   title: 'Elect.Gen - Project contributors',
