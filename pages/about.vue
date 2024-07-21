a128d503b2679944ca38759499cd0c07257ab257
acecution <acecution7@gmail.com>
2024-07-21 15:56:44 +0300

    update: relative styling

--- pages/about.vue
+++ pages/about.vue
@@ -1,123 +1,141 @@
 <script lang="ts" setup>
+import avatar1 from '~/assets/images/avatar1.jpg'
+import avatar2 from '~/assets/images/avatar2.jpg'
+import avatar3 from '~/assets/images/avatar3.jpg'
+import avatar4 from '~/assets/images/avatar4.jpg'
+import avatar5 from '~/assets/images/avatar5.png'
+import avatar6 from '~/assets/images/avatar6.jpg'
+import avatar7 from '~/assets/images/avatar7.jpg'
+import avatar8 from '~/assets/images/avatar8.jpg'
+import Member from '~/components/shared/Member.vue'
 import Header from '~/components/widgets/Header.vue'
+
+const teamMembers = [
+  {
+    image: avatar1,
+    alt: 'evgenii-photo',
+    link: 'https://github.com/ebortsov',
+    name: 'Evgenii Bortsov',
+    role: 'UX/UI Designer'
+  },
+  {
+    image: avatar2,
+    alt: 'anastasia-photo',
+    link: 'https://github.com/acecution',
+    name: 'Anastasia Varfolomeeva',
+    role: 'Front-end'
+  },
+  {
+    image: avatar3,
+    alt: 'kirill-photo',
+    link: 'https://github.com/lanebo1',
+    name: 'Kirill Efimovich',
+    role: 'Algorithm Developer'
+  },
+  {
+    image: avatar4,
+    alt: 'ilya-linh-sdr-photo',
+    link: 'https://github.com/Pickpusha',
+    name: 'Ilya-Linh Nguen',
+    role: 'DevOps'
+  },
+  {
+    image: avatar5,
+    alt: 'maxim-photo',
+    link: 'https://github.com/newspec',
+    name: 'Maxim Malov',
+    role: 'Algorithm Developer'
+  },
+  {
+    image: avatar6,
+    alt: 'arsen-photo',
+    link: 'https://github.com/projacktor',
+    name: 'Arsen Galiev',
+    role: 'Full-stack'
+  },
+  {
+    image: avatar7,
+    alt: 'asqar-photo',
+    link: 'https://github.com/asqarslanov',
+    name: 'Asqar Arslanov',
+    role: 'Full-stack'
+  },
+  {
+    image: avatar8,
+    alt: 'sarmat-photo',
+    link: 'https://github.com/1sarmatt',
+    name: 'Sarmat Lutfullin',
+    role: 'Front-end'
+  }
+]
 </script>
 
 <template>
   <Header />
-  <main>
-    <div class="relative h-full w-full pl-5 pt-20">
-      <div class="absolute mt-[10rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/ebortsov"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="evgenii-photo" src="~/assets/images/avatar1.jpg" />
-          </div>
-          <p class="text-center font-bold">Evgenii Bortsov</p>
-          <p class="text-center">UX/UI Designer</p>
-        </a>
-      </div>
-      <div class="absolute ml-[90rem] mt-[10rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/acecution"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="anastasia-photo" src="~/assets/images/avatar2.jpg" />
-          </div>
-          <p class="text-center font-bold">Anastasia Varfolomeeva</p>
-          <p class="text-center">Front-end</p>
-        </a>
-      </div>
-      <div class="absolute ml-[45rem] mt-[10rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/lanebo1"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="kirill-photo" src="~/assets/images/avatar3.jpg" />
-          </div>
-          <p class="text-center font-bold">Kirill Efimovich</p>
-          <p class="text-center">Algorithm Developer</p>
-        </a>
-      </div>
-      <div class="absolute ml-[25rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/Pickpusha"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="ilya-linh-sdr-photo" src="~/assets/images/avatar4.jpg" />
-          </div>
-          <p class="text-center font-bold">Ilya-Linh Nguen</p>
-          <p class="text-center">DevOps</p>
-        </a>
-      </div>
-      <div class="absolute ml-[65rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/newspec"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="maxim-photo" src="~/assets/images/avatar5.png" />
-          </div>
-          <p class="text-center font-bold">Maxim Malov</p>
-          <p class="text-center">Algorithm Developer</p>
-        </a>
-      </div>
-      <div class="absolute ml-[25rem] mt-[25rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/projacktor"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="arsen-photo" src="~/assets/images/avatar6.jpg" />
-          </div>
-          <p class="text-center font-bold">Arsen Galiev</p>
-          <p class="text-center">Full-stack</p>
-        </a>
-      </div>
-      <div class="absolute ml-[65rem] mt-[25rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/asqarslanov"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="asqar-photo" src="~/assets/images/avatar7.jpg" />
-          </div>
-          <p class="text-center font-bold">Asqar Arslanov</p>
-          <p class="text-center">Full-stack</p>
-        </a>
-      </div>
-      <div class="absolute ml-[45rem] mt-[35rem] h-[8vw] w-[8vw] flex-col items-center">
-        <a
-          class="transition duration-200 ease-in-out hover:text-green-500"
-          href="https://github.com/1sarmatt"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <div class="app mb-[10px] h-[8vw] w-[8vw] rounded-full">
-            <img class="rounded-full" alt="sarmat-photo" src="~/assets/images/avatar8.jpg" />
-          </div>
-          <p class="text-center font-bold">Sarmat Lutfullin</p>
-          <p class="text-center">Front-end</p>
-        </a>
-      </div>
+  <main class="grid grid-rows-4 gap-5">
+    <div class="flex flex-row items-center space-x-72 pl-72">
+      <Member
+        :alt="teamMembers[3].alt"
+        :image="teamMembers[3].image"
+        :link="teamMembers[3].link"
+        :name="teamMembers[3].name"
+        :role="teamMembers[3].role"
+      />
+      <Member
+        :alt="teamMembers[4].alt"
+        :image="teamMembers[4].image"
+        :link="teamMembers[4].link"
+        :name="teamMembers[4].name"
+        :role="teamMembers[4].role"
+      />
+    </div>
+    <div class="flex flex-row items-center space-x-72 pl-72">
+      <Member
+        :alt="teamMembers[0].alt"
+        :image="teamMembers[0].image"
+        :link="teamMembers[0].link"
+        :name="teamMembers[0].name"
+        :role="teamMembers[0].role"
+      />
+      <Member
+        :alt="teamMembers[2].alt"
+        :image="teamMembers[2].image"
+        :link="teamMembers[2].link"
+        :name="teamMembers[2].name"
+        :role="teamMembers[2].role"
+      />
+      <Member
+        :alt="teamMembers[1].alt"
+        :image="teamMembers[1].image"
+        :link="teamMembers[1].link"
+        :name="teamMembers[1].name"
+        :role="teamMembers[1].role"
+      />
+    </div>
+    <div class="flex flex-row items-center space-x-72 pl-72">
+      <Member
+        :alt="teamMembers[5].alt"
+        :image="teamMembers[5].image"
+        :link="teamMembers[5].link"
+        :name="teamMembers[5].name"
+        :role="teamMembers[5].role"
+      />
+      <Member
+        :alt="teamMembers[6].alt"
+        :image="teamMembers[6].image"
+        :link="teamMembers[6].link"
+        :name="teamMembers[6].name"
+        :role="teamMembers[6].role"
+      />
+    </div>
+    <div class="flex flex-row items-center pl-72">
+      <Member
+        :alt="teamMembers[7].alt"
+        :image="teamMembers[7].image"
+        :link="teamMembers[7].link"
+        :name="teamMembers[7].name"
+        :role="teamMembers[7].role"
+      />
     </div>
   </main>
 </template>
