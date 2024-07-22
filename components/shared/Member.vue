ed3a0b1e50005698d89f89237ba412cfa4f77720
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-22 11:27:04 +0300

    style: apply Prettier

--- components/shared/Member.vue
+++ components/shared/Member.vue
@@ -1,18 +1,18 @@
 <template>
   <button>
-  <a
-    class="flex flex-col items-center transition duration-200 ease-in-out hover:text-green-500"
-    :href="link"
-    rel="noopener noreferrer"
-    target="_blank"
-  >
-    <div class="h-24 w-24 rounded-full">
-      <img class="rounded-full" :alt="alt" :src="image" />
-    </div>
-    <p class="text-center font-bold">{{ name }}</p>
-    <p class="text-center">{{ role }}</p>
-  </a>
-    </button>
+    <a
+      class="flex flex-col items-center transition duration-200 ease-in-out hover:text-green-500"
+      :href="link"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <div class="h-24 w-24 rounded-full">
+        <img class="rounded-full" :alt="alt" :src="image" />
+      </div>
+      <p class="text-center font-bold">{{ name }}</p>
+      <p class="text-center">{{ role }}</p>
+    </a>
+  </button>
 </template>
 
 <script lang="ts" setup>
